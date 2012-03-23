4cc05854cdc8f6ad196e06e322b5ff0d4e9a55ed
Kyle Drake <kyledrake@gmail.com>
2012-03-23 13:07:34 -0700

    Add accessors for code and transaction_id. 0.0.6

--- lib/locaid/api_error.rb
+++ lib/locaid/api_error.rb
@@ -1,5 +1,6 @@
 module Locaid
   class ApiError < StandardError
+    attr_reader :code, :transaction_id
     def initialize(code, message, transaction_id)
       @code, @message, @transaction_id = code, message, transaction_id
       super "#{message} (code #{code})"
